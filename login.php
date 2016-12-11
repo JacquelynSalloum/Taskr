cd7b829e44c822d57c1ac9771463adfb793d7fa2
nickmacdonald7 <nickmacdonald7@gmail.com>
2016-12-11 15:26:37 -0400

    Small changes made to css for rating, and login credentials for db

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 
 //credentials for login.php
-$db_hostname = "localhost";
+$db_hostname = "db.cs.dal.ca";
 $db_database = "macdona5";
 $db_username = "macdona5";
 $db_password = "B00668638";
